2be2414c19909806bc2beedcb926860c71c572be
marianochavez <amarianochavez@gmail.com>
2021-04-20 11:49:11 -0300

    to_representation/password encryption create-update

--- apps/users/api/api.py
+++ apps/users/api/api.py
@@ -1,4 +1,4 @@
-from apps.users.api.serializers import UserSerializer,TestUserSerializer
+from apps.users.api.serializers import UserSerializer,UserListSerializer
 from apps.users.models import User
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
@@ -11,8 +11,8 @@ def user_api_view(request):
     #list
     if request.method == 'GET':
         #queryset
-        users = User.objects.all()
-        users_serializer = UserSerializer(users, many = True) #many=false-->serialize only one
+        users = User.objects.all().values('id','username','email','password')
+        users_serializer = UserListSerializer(users, many = True) #many=false-->serialize only one
 
         return Response(users_serializer.data,status = status.HTTP_200_OK)
     
@@ -40,7 +40,7 @@ def user_detail_api_view(request,pk=None):
 
         #update
         elif request.method == 'PUT':
-            user_serializer = TestUserSerializer(user, data = request.data)
+            user_serializer = UserSerializer(user, data = request.data)
             if user_serializer.is_valid():
                 user_serializer.save()
                 return Response(user_serializer.data,status = status.HTTP_200_OK)
@@ -49,6 +49,6 @@ def user_detail_api_view(request,pk=None):
         #delete
         elif request.method == 'DELETE':
             user.delete()
-            return Response({'message':'Usuario eliminado correctamente!'},status = status.HTTP_201_OK)
+            return Response({'message':'Usuario eliminado correctamente!'},status = status.HTTP_200_OK)
     
     return Response({'message':'No se ha encontrado ningun usuario con estos datos'},status = status.HTTP_400_BAD_REQUEST)
--- apps/users/api/serializers.py
+++ apps/users/api/serializers.py
@@ -6,34 +6,27 @@ class UserSerializer(serializers.ModelSerializer):
         model = User
         fields = '__all__'
 
-class TestUserSerializer(serializers.Serializer):
-    name = serializers.CharField(max_length=200)
-    email = serializers.EmailField()
-
-    def validate_name(self,value):
-        #custom validation
-        if 'develop' in value:
-            raise serializers.ValidationError('Error, no puede llamarse develop')
-        
-        return value
-    
-    def validate_email(self,value):
-        #custom validation
-        if value == '':
-            raise serializers.ValidationError('Tiene que indicar un correo')
-
-        return value
-    
-    def validate(self,data):
-        if data['name'] in data['email']:
-            raise serializers.ValidationError('El email no puede contener el usuario')
-        return data
-
     def create(self,validated_data):
-        return self.model.objects.create(**validated_data)
+        #encrypted password
+        user = User(**validated_data)
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
 
     def update(self,instance,validated_data):
-        instance.name = validated_data.get('name',instance.name)
-        instance.email = validated_data.get('email',instance.email)
-        instance.save()
-        return instance
+        updated_user = super().update(instance,validated_data)
+        updated_user.set_password(validated_data['password'])
+        updated_user.save()
+        return updated_user
+
+class UserListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+
+    def to_representation(self,instance):
+        return {
+            'id': instance['id'],
+            'username': instance['username'],
+            'email': instance['email'],
+            'password': instance['password']
+        }

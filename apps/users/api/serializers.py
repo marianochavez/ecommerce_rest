f22c4ee1b33515dce3722c1cb61e81248ab6a6e6
marianochavez <amarianochavez@gmail.com>
2021-04-20 10:21:37 -0300

    create serializer/validations

--- apps/users/api/serializers.py
+++ apps/users/api/serializers.py
@@ -5,3 +5,35 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = '__all__'
+
+class TestUserSerializer(serializers.Serializer):
+    name = serializers.CharField(max_length=200)
+    email = serializers.EmailField()
+
+    def validate_name(self,value):
+        #custom validation
+        if 'develop' in value:
+            raise serializers.ValidationError('Error, no puede llamarse develop')
+        
+        return value
+    
+    def validate_email(self,value):
+        #custom validation
+        if value == '':
+            raise serializers.ValidationError('Tiene que indicar un correo')
+        
+        if self.validate_name(self.context['name']) in value:
+            raise serializers.ValidationError('El email no puede contener el nombre')
+        
+        return value
+    
+    def validate(self,data):
+        if data['name'] in data['email']:
+            raise serializers.ValidationError('El email no puede contener el usuario')
+        return data
+
+    def create(self,validated_data):
+        print(validated_data)
+        return User.objects.create(**validated_data)
+
+    

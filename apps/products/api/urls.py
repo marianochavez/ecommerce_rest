2a1cbe42a0aabdcf126d920fb70b1a429060fb28
marianochavez <amarianochavez@gmail.com>
2021-04-24 20:12:24 -0300

    Implement RetrieveUpdateDestroyAPIView in product_views

--- apps/products/api/urls.py
+++ apps/products/api/urls.py
@@ -1,14 +1,11 @@
 from django.urls import path
 from apps.products.api.views.general_views import MeasureUnitListAPIView,IndicatorListAPIView,CategoryProductListAPIView
-from apps.products.api.views.product_views import ProductListCreateAPIView,ProductRetrieveAPIView
-from apps.products.api.views.product_views import ProductRetrieveDestroyAPIView,ProductRetrieveUpdateAPIView
+from apps.products.api.views.product_views import ProductListCreateAPIView,ProductRetrieveUpdateDestroyAPIView
 
 urlpatterns = [
     path("measure_unit/", MeasureUnitListAPIView.as_view(), name="measure_unit"),
     path("indicator/", IndicatorListAPIView.as_view(), name="indicator"),
     path("category_product/", CategoryProductListAPIView.as_view(), name="category_product"),
     path("product/", ProductListCreateAPIView.as_view(), name="product_create"),
-    path("product/retrieve/<int:pk>", ProductRetrieveAPIView.as_view(), name="product_retrieve"),
-    path("product/destroy/<int:pk>", ProductRetrieveDestroyAPIView.as_view(), name="product_destroy"),
-    path("product/update/<int:pk>", ProductRetrieveUpdateAPIView.as_view(), name="product_update"),
+    path("product/rud/<int:pk>", ProductRetrieveUpdateDestroyAPIView.as_view(), name="product_rud"),
 ]
--- apps/products/api/views/product_views.py
+++ apps/products/api/views/product_views.py
@@ -16,75 +16,56 @@ class ProductListCreateAPIView(generics.ListCreateAPIView):
         return self.get_serializer().Meta.model.objects.filter(state=True)
 
     def post(self,request):
+        """Create product method
+
+        """
         serializer = self.serializer_class(data = request.data)
         if serializer.is_valid():
             serializer.save()
             return Response({'message':'Producto creado correctamente'},status = status.HTTP_201_CREATED)
         return Response(serializer.errors,status = status.HTTP_400_BAD_REQUEST)
 
-class ProductRetrieveAPIView(generics.RetrieveAPIView):
-    """Its like list api view but only gets one product with his pk
+class ProductRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
+    """ Concrete view for retrieving, updating or deleting a model instance of Product
 
     """
     serializer_class = ProductSerializer
 
-    def get_queryset(self):
-        return self.get_serializer().Meta.model.objects.filter(state=True)
-
-class ProductRetrieveDestroyAPIView(generics.RetrieveDestroyAPIView):
-    """Delete Product, I change generics.DestroyAPIView to generics.RetrieveDestroyAPIView
-        to see the attributes of the product I want to delete
-
-    """
-    serializer_class = ProductSerializer
-
-    def get_queryset(self):
-        """Get the model name with self.get_serializer().Meta.model
-            and filter state true
-        """
-        return self.get_serializer().Meta.model.objects.filter(state=True)
-
-    def delete(self,request,pk=None):
-        """Modified to soft delete, only change the state of a product
-
-        """
-        product = self.get_queryset().filter(id=pk).first()
-        if product:
-            product.state = False
-            product.save()
-            return Response({'message':'Producto eliminado correctamente!'},status = status.HTTP_200_OK)
-        return Response({'error':'No existe un producto con estos datos!'},status = status.HTTP_400_BAD_REQUEST)
-
-class ProductRetrieveUpdateAPIView(generics.RetrieveUpdateAPIView):
-    """Update product and see his attributes with retrieve
-
-    """
-    serializer_class = ProductSerializer
-
-    def get_queryset(self):
-        """Get the model name with self.get_serializer().Meta.model
-            and filter state true
-        """
-        return self.get_serializer().Meta.model.objects.filter(state=True)
-
-    """
-    Example of method path and put, unused
-
-    def get_queryset(self,pk):
-        return self.get_serializer().Meta.model.objects.filter(state = True).filter(id = pk).first()
+    def get_queryset(self,pk=None):
+        if pk is None:
+            return self.get_serializer().Meta.model.objects.filter(state = True)
+        else:
+            return self.get_serializer().Meta.model.objects.filter(id = pk,state = True).first()
 
     def patch(self,request,pk=None):
+        """Gets an instance of product, refill instance attributes in raw data (json)
+
+        """
         if self.get_queryset(pk):
             product_serializer = self.serializer_class(self.get_queryset(pk)) # serialize instance
             return Response(product_serializer.data,status = status.HTTP_200_OK)
         return Response({'error':'No existe un Producto con estos datos!'},status = status.HTTP_400_BAD_REQUEST)
 
     def put(self,request,pk=None):
+        """Update product method
+
+        """
         if self.get_queryset(pk):
             product_serializer = self.serializer_class(self.get_queryset(pk),data = request.data) # send information to serializer referencing the instance
             
-            if product_serializer.is_valid(): # serializer validations
+            if product_serializer.is_valid():
                 product_serializer.save()
                 return Response(product_serializer.data,status = status.HTTP_200_OK)
             return Response(product_serializer.errors,status = status.HTTP_400_BAD_REQUEST)
-    """
+
+    def delete(self,request,pk=None):
+        """Modified to soft delete, only change the state of a product
+
+        """
+        product = self.get_queryset().filter(id = pk).first() # get instance
+        
+        if product:
+            product.state = False
+            product.save()
+            return Response({'message':'Producto eliminado correctamente!'},status = status.HTTP_200_OK)
+        return Response({'error':'No existe un Producto con estos datos!'},status = status.HTTP_400_BAD_REQUEST)
